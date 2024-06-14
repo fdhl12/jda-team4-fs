1a2ce6fffc990dcedcd75a4588f08662b2b39b5e
irfanykywz <irfanykywz@gmail.com>
2024-06-14 10:44:06 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,28 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Route::get('/admin', function (){
-   return view('admin.dashboard');
-})->name('admin.dashboard');
 
-Route::get('/admin/form', function (){
-    return view('admin.form');
-})->name('admin.form');
+Route::middleware([])->group(function (){
 
-Route::get('/admin/table', function (){
-    return view('admin.table');
-})->name('admin.table');
+    Route::get('/admin', function (){
+       return view('admin.dashboard');
+    })->name('admin.dashboard');
 
-Route::get('/admin/tabs', function (){
-    return view('admin.tabs');
-})->name('admin.tabs');
+    Route::get('/admin/form', function (){
+        return view('admin.form');
+    })->name('admin.form');
 
-Route::get('/admin/wysiwyg', function (){
-    return view('admin.wysiwyg');
-})->name('admin.wysiwyg');
+    Route::get('/admin/table', function (){
+        return view('admin.table');
+    })->name('admin.table');
+
+    Route::get('/admin/tabs', function (){
+        return view('admin.tabs');
+    })->name('admin.tabs');
+
+    Route::get('/admin/wysiwyg', function (){
+        return view('admin.wysiwyg');
+    })->name('admin.wysiwyg');
+
+});
+
