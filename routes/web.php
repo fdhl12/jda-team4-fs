2e56ee46a1c1f09869c701e0ce07e5943f7fae24
irfanykywz <irfanykywz@gmail.com>
2024-06-15 11:41:52 +0700

    login page

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware([])->group(function (){
+Route::middleware(['auth'])->group(function (){
 
     Route::get('/admin', function (){
        return view('admin.dashboard');
