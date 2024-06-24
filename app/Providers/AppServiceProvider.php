a83368819a3964694a407d40af5815a54205b4e6
irfanykywz <irfanykywz@gmail.com>
2024-06-24 10:49:37 +0700

    fix pengaturan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Pengaturan;
 use Laravel\Sanctum\Sanctum;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\PersonalAccessToken;
@@ -23,12 +22,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
-
-        /* mendapatkan konfigurasi dari table pengaturan */
-        foreach (Pengaturan::all(['type', 'data']) as $read)
-        {
-            $pengaturan[$read['type']] = $read['data'];
-        }
-        view()->share('pengaturan', $pengaturan);
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <a href="{{ route('home') }}" class="flex items-center mb-6 text-2xl font-bold text-black
             -900 dark:text-white">
-                <img class="w-16 h-16 mr-2" src="{{ asset('images/logo.png') }}" alt="logo">
+                <img class="w-16 h-16 mr-2" src="{{ asset($pengaturan['logo']) }}" alt="logo">
                 {{ config('app.name', 'Laravel') }}
             </a>
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
